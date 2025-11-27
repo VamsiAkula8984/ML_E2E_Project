81d10eb11d286b46f1fae9a1bc924970857d6dba
VamsiAkula8984 <advvamsi@gmail.com>
2025-11-27 15:59:28 -0500

    Data Transformation pipelines added
    In addition, some re-usable methods were added in utils.py

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -7,9 +7,15 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
-#configures where train, test data is saved
+from src.components.data_transformation import DataTransformation
+
+
+#@dataclass is a decorator in Python (from the dataclasses module)
+#  that automatically generates common, boilerplate code for classes 
+# that are mainly used to store data.
 @dataclass
 class DataIngestionConfig:
+    #configures where train, test data is saved
     train_data_path:str = os.path.join('artifacts', 'train.csv')
     test_data_path:str = os.path.join('artifacts', 'test.csv')
     raw_data_path:str = os.path.join('artifacts', 'data.csv')
@@ -51,6 +57,10 @@ class DataIngestion:
         except Exception as e:
             raise CustomException(e,sys)
 
+
 if __name__=="__main__":
     obj=DataIngestion()
     train_data,test_data=obj.initiate_data_ingestion()
+
+    data_transformation=DataTransformation()
+    train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
